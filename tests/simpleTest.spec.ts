e656edb58ddc912196d4dd3f51c684cc5333045c
Aleksandr Zyabrev <aleksandr_zyabrev@epam.com>
2022-06-21 17:10:22 +0300

    update

--- tests/simpleTest.spec.ts
+++ tests/simpleTest.spec.ts
@@ -1,7 +1,7 @@
 import { expect, Page, test } from "@playwright/test";
 
 test("Sign In Button should be visible on the login page", async ({ page }) => {
-    await page.goto('/');
+    await page.goto('https://playwright.dev/');
     const signInButton = page.locator("button", { hasText: "Sign in" });
     await expect(signInButton).toBeEnabled();
 });
